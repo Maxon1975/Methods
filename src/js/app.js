8926da5949cfeb3a4237e4c871480d54db1e5286
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2023-08-13 17:31:42 +0300

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import Bowman from './bowman';
+import Swordsman from './swordsman';
+import Magician from './magician';
+import Daemon from './daemon';
+import Undead from './undead';
+import Zombie from './zombie';
+
+console.log('app worked');
+
+const bowman = new Bowman('Лучник');
+const swordsman = new Swordsman('Мечник');
+const magician = new Magician('Маг');
+const daemon = new Daemon('Демон');
+const undead = new Undead('Вампир');
+const zombie = new Zombie('Зомби');
